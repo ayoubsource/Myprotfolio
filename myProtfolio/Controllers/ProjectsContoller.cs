47100867fc56297b9854e8ffad1d0ea86b1fcc83
ayoubsource <ayoub.chaabi01@gmail.com>
2023-06-16 21:39:29 +0200

    Init project

---
+++ myProtfolio/Controllers/ProjectsContoller.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using myProtfolio.Models;
+
+namespace myProtfolio.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Details(int id)
+        {
+            var project = GetProjectById(id);
+            return  View("~/Views/Projects/Details.cshtml",project);
+        }
+
+        // Helper method to retrieve the skill details from your data source
+        private Project GetProjectById(int id)
+        {
+            // Implement your logic to fetch the skill details by ID
+            // This can be from a database, API, or any other data source
+
+            // Example: returning a mock skill object for demonstration
+            var project = new Project
+            {
+                Id = id,
+                Name = "HTML",
+                Description = "HyperText Markup Language"
+            };
+
+            return project;
+        }
+    }
+}
---
+++ myProtfolio/Models/Project.cs
@@ -0,0 +1,9 @@
+﻿namespace myProtfolio.Models
+{
+    internal class Project
+    {
+        public int Id { get; internal set; }
+        public string Name { get; internal set; }
+        public string Description { get; internal set; }
+    }
+}
