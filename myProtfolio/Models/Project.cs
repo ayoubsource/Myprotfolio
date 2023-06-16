f2b2b973aed255b7717bef9c5e3d37522b01257a
ayoubsource <ayoub.chaabi01@gmail.com>
2023-06-16 21:39:29 +0200

    Init project

---
+++ myProtfolio/Models/Project.cs
@@ -0,0 +1,9 @@
+﻿namespace myProtfolio.Models
+{
+    public  class Project
+    {
+        public int Id { get; internal set; }
+        public string Name { get; internal set; }
+        public string Description { get; internal set; }
+    }
+}
---
+++ myProtfolio/Repositories/ProjectsRepository.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using myProtfolio.Models;
+
+namespace myProtfolio.Repositories
+{
+    public class ProjectsRepository : DbContext
+    {
+
+        public DbSet<Project> Projects { get; set; }
+
+
+       public ProjectsRepository(DbContextOptions<ProjectsRepository> options) : base(options)
+    {
+            Projects = Set<Project>();
+    }
+
+    public bool AddNewProject(Project project)
+        {
+            if(project == null)
+            {
+                return false;
+            }
+            Projects.Add(project);
+            return true;
+        }
+        public DbSet<Project> GetAllProjects()
+        {
+            return Projects;
+        }
+
+        public Project GetProjectById(long id)
+        {
+            if (id < 0)
+                throw new ArgumentException("Id could not be null");
+            return Projects.Find(id);
+        }
+    }
+}
